8e4907eb38c95c9666af42c9d7f7a10979e45fb3
Bruno Gianini <bruno.l.gianini@avanade.com>
2024-11-23 10:03:59 -0300

    test: sonarcloud no workflow java

--- java/src/main/java/com/exaple/App.java
+++ java/src/main/java/com/exaple/App.java
@@ -3,6 +3,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
     }
 }
--- java/src/test/java/com/exaple/AppTest.java
+++ java/src/test/java/com/exaple/AppTest.java
@@ -1,19 +1,20 @@
-package com.exaple;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+package com.example;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest {
+class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    void testMain() {
+        // Redireciona a saída para verificar o que foi impresso
+        var outContent = new java.io.ByteArrayOutputStream();
+        System.setOut(new java.io.PrintStream(outContent));
+        
+        // Chama o método main da classe App
+        App.main(new String[]{});
+        
+        // Verifica se a saída é "Hello, World!" seguida de uma nova linha
+        assertEquals("Hello, World!", outContent.toString());
     }
 }
