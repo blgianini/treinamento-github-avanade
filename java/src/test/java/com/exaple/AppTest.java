78986fb2ecea0e72bdd636ed909aa507037d84bd
Bruno Gianini <bruno.l.gianini@avanade.com>
2024-11-23 14:23:20 -0300

    test: sonarcloud no workflow java

--- java/src/test/java/com/exaple/AppTest.java
+++ java/src/test/java/com/exaple/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+    
     @Test
     void testMain() {
         // Redireciona a saída para verificar o que foi impresso
